03e97c17c1bb698dd144970e7f4c25528ff7e9ec
Codefident <piotr.klep01@wp.pl>
2021-11-01 15:35:40 +0100

    z03

---
+++ cw3/z03.py
@@ -0,0 +1,19 @@
+def sieve(n):
+    prime = [True for _ in range(n + 1)]
+
+    i = 2
+    while i**2 <= n:
+        if prime[i] == True:
+            j = 2
+            while j*i <= n:
+                prime[j*i] = False
+                j += 1
+        i += 1
+    
+    for i, item in enumerate(prime):
+        if item and i not in (0, 1):
+            print(i, end=" ")
+
+
+n = int(input("n = "))
+sieve(n)
