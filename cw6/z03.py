c1fe186c3572ec28d0233b94cb5cada61731d39d
Codefident <piotr.klep01@wp.pl>
2021-12-11 01:27:34 +0100

    z03 in both versions added

--- cw6/z03.py
+++ cw6/z03.py
@@ -8,29 +8,46 @@ from random import randint
 
 
 SIZE = 8
-T = [[randint(1, 10) for _ in range(SIZE)] for _ in range(SIZE)]
+# T = [[randint(1, 9) for _ in range(SIZE)] for _ in range(SIZE)]
 k = 1
 moves = ((1, 1), (1, 0), (1, 1))
-cost = 80
+T = [[6, 7, 6, 6, 8, 6, 1, 6],
+[6, 3, 2, 9, 8, 3, 6, 6],
+[2, 6, 2, 8, 2, 6, 5, 9],
+[9, 3, 3, 3, 9, 1, 5, 7],
+[9, 7, 9, 8, 9, 1, 1, 8],
+[1, 2, 6, 6, 9, 6, 7, 9],
+[1, 9, 5, 5, 2, 2, 8, 9],
+[2, 9, 5, 8, 2, 9, 5, 7]]
 
 
-def move(w, k, s_cost):
+def move(w, k, s_cost, t):
+    global cost
     s_cost += T[w][k]
-    
+    if w==7:
+        print(w, k, s_cost, t)
+    t.append(s_cost)
     if w == 7:
-        return s_cost
-            
+        if s_cost < cost:
+            cost = s_cost
+        return
+
     for a, b in moves:
         if k + b in range(SIZE):
-            move(a, k + b, s_cost)
+            move(w + a, k + b, s_cost, t.copy())
 
 
 def start(T, k):
+    global cost
+    cost = 80
     cost_st = T[0][k]
-
+    print(T[0][k])
     for a, b in moves:
         if k + b in range(SIZE):
-            move(a, k + b, cost_st)
+            move(a, k + b, cost_st, [cost_st])
+    print(cost)
 
 
+for line in T:
+    print(line)
 start(T, k)
