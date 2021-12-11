999be9f160083c0b3c5ff18060f848a3b0f9cc2f
Codefident <piotr.klep01@wp.pl>
2021-12-11 01:27:17 +0100

    z04 fixed

--- cw6/z04.py
+++ cw6/z04.py
@@ -3,38 +3,33 @@
 # wymiarach NxN ruchem skoczka szachowego.
 
 
-N = 5
-board = [[0 for _ in range(N)] for _ in range(N)]
+N = 8
+t = [[0 for _ in range(N)] for _ in range(N)]
 jumps = ((-1, 2), (1, 2), (2, 1), (2, -1),
             (1, -2), (-1, -2), (-2, -1), (-2, 1))
-all_jumps = [None for _ in range(N*N)]
-r = len(jumps)
 cnt = 1
 l, c = 0, 0  # start point
 
 
 def jump(l, c, cnt):
-    board[l][c] = cnt
-
-    for i in range(r):
-        a = jumps[i][0]
-        b = jumps[i][1]
-        if l+a >= 0 and l+a < N and c+b >= 0 and c+b < N:
-            if board[l+a][c+b] == 0:
-                all_jumps[cnt-1] = i
+    t[l][c] = cnt
+    
+    for a, b in jumps:
+        if l+a in range(N) and c+b in range(N):
+            if t[l+a][c+b] == 0:
                 jump(l+a, c+b, cnt+1)
 
-    if cnt**2 < N:
-        board[l][c] = 0
-        all_jumps[cnt-1]
-        jump(l-all_jumps[cnt-2][0], c-all_jumps[cnt-2][1], cnt-1)
+    if cnt < N**2:
+        t[l][c] = 0
+        return
 
     else:
-        for line in board:
+        for line in t:
             print("[ ", end="")
             for elem in line:
                 print(elem, "\t", end="")
             print(" ]")
+        return
 
 
 jump(l, c, cnt)
