df0148cdf5f90efee3d64bcd698d29adbe2d8407
Codefident <piotr.klep01@wp.pl>
2022-01-18 21:24:41 +0100

    z18 fixed

--- cw7/z18.py
+++ cw7/z18.py
@@ -16,16 +16,18 @@ def printList(h):
 
 ll = Node(1)
 p = ll
-for i in [2, 3, 1, 7, 1, 2, 3, 5, 7, 18, 3, 5, 22]:
+for i in [2, 33, 3, 1, 7, 5, 17, 1, 2, 3, 5, 7, 18, 3, 5, 22]:
     p.next = Node(i)
     p = p.next
 
 
 def uniq(h):
     p = h
+    last_uniq = h
     prev_t = None
     t = None
-    del_h = False # usuwanie pierwszego elementu jesli sie powtarza
+    del_h = False  # usuwanie pierwszego elementu jesli sie powtarza
+    del_p = False  # usuwanie elementu do ktorego porownujemy reszte
 
     while p is not None:
         prev_t = p
@@ -33,7 +35,9 @@ def uniq(h):
 
         while t is not None:
             if t.value == p.value:
-                del_h = True
+                del_p = True
+                if p == h:
+                    del_h = True
                 prev_t.next = t.next
             else:
                 prev_t = t
@@ -43,9 +47,15 @@ def uniq(h):
             h = p.next
             del_h = False
 
+        if del_p:
+            last_uniq.next = p.next
+            del_p = False
+        else:
+            last_uniq = p
+
         p = p.next
     return h
-    
+
 
 printList(ll)
 ll = uniq(ll)
